b14a6e8efd665909325843e32bb4ea45952a74c1
AlanW95 <alan.williams.1995@gmail.com>
2019-08-01 18:33:43 +0100

    Small Changes

---
+++ LogInRegister/Assets/Scripts/1 - Balance/ClickHover/Interview2CH.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interview2CH : MonoBehaviour
+{
+
+    public GameObject interviewTool;
+
+    void OnMouseDown() {
+        SceneManager.LoadScene("LeaverInterview");    
+    }
+
+    private void OnMouseOver() {
+        Debug.Log("You are hovering over this object.");
+
+        interviewTool.SetActive(true); ;
+    }
+
+    private void OnMouseExit() {
+        interviewTool.SetActive(false);
+    }
+}
--- LogInRegister/Assets/Scripts/1 - Balance/ClickHover/NewsflashCH.cs
+++ LogInRegister/Assets/Scripts/1 - Balance/ClickHover/NewsflashCH.cs
@@ -1,104 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class NewsflashCH : MonoBehaviour
 {
-    public GameObject mainCamera;
-    public GameObject finalAssignmentCamera;
-
-    public GameObject newsflash;
     public GameObject newsflashBar;
-
-    public GameObject[] pages;
 
     public GameObject newsflashText;
 
-    public GameObject backButton;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        mainCamera.SetActive(true);
-        finalAssignmentCamera.SetActive(false);
-        backButton.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if the Interview and Discussion tool are complete, the news flash bar will appear
-    }
-
-    void OnMouseDown() {
-        //SceneManager.LoadScene("FinalAssignment"); //no longer doing this until camera is zoomed in
-        mainCamera.SetActive(false);
-        newsflashText.SetActive(true);
-        backButton.SetActive(true);
-    }
-
-    void OnMouseHover() {
-        Debug.Log("You are hovering over this laptop!");
-
+    public void OnPointerEnter(PointerEventData eventData) {
         newsflashText.SetActive(true);
     }
 
-    void OnMouseExit() {
+    public void OnPointerClick(PointerEventData eventData) {
+        SceneManager.LoadScene("Newsflash");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
         newsflashText.SetActive(false);
     }
 
     public void DisplayNewsflash() {
-        newsflashBar.SetActive(false);
-        newsflash.SetActive(true);
-        backButton.SetActive(true);
-    }
-
-    public void ReturnToMobileWorkplace() {
-        pages[0].SetActive(false);
-        pages[1].SetActive(false);
-        pages[2].SetActive(false);
-        pages[3].SetActive(false);
-        pages[4].SetActive(false);
-        backButton.SetActive(false);
-    }
-
-    public void Page1() {
-        pages[0].SetActive(true);
-        pages[1].SetActive(false);
-        pages[2].SetActive(false);
-        pages[3].SetActive(false);
-        pages[4].SetActive(false);
-    }
-
-    public void Page2() {
-        pages[0].SetActive(false);
-        pages[1].SetActive(true);
-        pages[2].SetActive(false);
-        pages[3].SetActive(false);
-        pages[4].SetActive(false);
-    }
-
-    public void Page3() {
-        pages[0].SetActive(false);
-        pages[1].SetActive(false);
-        pages[2].SetActive(true);
-        pages[3].SetActive(false);
-        pages[4].SetActive(false);
-    }
-
-    public void Page4() {
-        pages[0].SetActive(false);
-        pages[1].SetActive(false);
-        pages[2].SetActive(false);
-        pages[3].SetActive(true);
-        pages[4].SetActive(false);
-    }
-
-    public void Page5() {
-        pages[0].SetActive(false);
-        pages[1].SetActive(false);
-        pages[2].SetActive(false);
-        pages[3].SetActive(false);
-        pages[4].SetActive(true);
+        SceneManager.LoadScene("Newsflash");
     }
 }

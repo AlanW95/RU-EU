226b2c1fe2afbb051a70d83c99172ce6ab9f9863
AlanW95 <alan.williams.1995@gmail.com>
2019-12-03 16:03:13 +0000

    Continuing Changes
    More changes to the game.

--- LogInRegister/Assets/Scripts/Introduction.cs
+++ LogInRegister/Assets/Scripts/Introduction.cs
@@ -7,17 +7,32 @@ using TMPro;
 
 public class Introduction : MonoBehaviour
 {
-    private string introductoryEN, introductoryGR;
+    private string introductoryEN, introductoryGR, introductoryDE, introductoryNL, introductoryHR;
     public TextMeshProUGUI introductoryText;
     private bool english = false;
     private bool greek = false;
+    private bool german = false;
+    private bool dutch = false;
+    private bool croatian = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        english = true;
-        greek = false;
+        english = intToBool(PlayerPrefs.GetInt("EnglishSelected"));
+        greek = intToBool(PlayerPrefs.GetInt("GreekSelected"));
+        german = intToBool(PlayerPrefs.GetInt("GermanSelected"));
+        dutch = intToBool(PlayerPrefs.GetInt("DutchSelected"));
+        croatian = intToBool(PlayerPrefs.GetInt("CroatianSelected"));
 
+        //english = true;
+        //greek = false;
+
+        //later languages
+        //german = false;
+        //dutch = false;
+        //croatian = false;
+
+        //Introductory Text for English being set in PlayerPrefs
         PlayerPrefs.SetString("IntroductoryEnglish", "Hello and welcome to RU EU, the educational game around issues of identity in Europe and the European Union. " +
             "\n\nThe game will begin with you going through the process of being “hired” as a journalist to complete an assignment around identity and Europe. " +
             "As such you will be asked a series of initial questions including some demographic data and attitudes about identity. " +
@@ -27,8 +42,19 @@ public class Introduction : MonoBehaviour
             "\n\nOnce you have completed all the scenarios, the game will then end with a series of questions around the issues of identity around Europe. Once you have answered these, you have completed the RU EU educational game. " +
             "\n\nThank you for selecting this game and good luck, we hope you enjoy playing it!");
 
+        //Introductory Text for Greek being set in PlayerPrefs
         PlayerPrefs.SetString("IntroductoryGreek", "Γεια σας και καλωσορίζουμε στην ΕΣ της ΕΕ, το εκπαιδευτικό παιχνίδι γύρω από θέματα ταυτότητας στην Ευρώπη και την Ευρωπαϊκή Ένωση. Το παιχνίδι θα ξεκινήσει από τη στιγμή που θα περάσετε τη διαδικασία να μισθώσετε ως δημοσιογράφος για να ολοκληρώσετε μια αποστολή γύρω από την ταυτότητα και την Ευρώπη. Ως εκ τούτου, θα σας ζητηθεί μια σειρά αρχικών ερωτήσεων, συμπεριλαμβανομένων μερικών δημογραφικών δεδομένων και στάσεων σχετικά με την ταυτότητα. Μετά από αυτές τις ερωτήσεις, θα προσληφθεί και στη συνέχεια θα σας ζητηθεί να συμπληρώσετε 5 σενάρια. Κάθε σενάριο παρέχει μια σειρά από καθήκοντα που επιλέγουν πληροφορίες από προσομοιωμένες συνεντεύξεις, newsfeeds, συζητήσεις και την ανάπτυξη τους σε ένα περίγραμμα για ένα άρθρο περιοδικού. Παρόλο που ποτέ δεν πρέπει να γράψετε το άρθρο στο παιχνίδι, πρέπει σε διάφορα σημεία κάθε σεναρίου να επιλέξετε πληροφορίες που θα μπορούσαν να συμπεριληφθούν στο ίδιο το άρθρο. Στο τέλος κάθε σεναρίου, θα πρέπει να επιλέξετε και να βελτιώσετε περαιτέρω τις πληροφορίες που έχετε επιλέξει σε αυτό το σενάριο. Αφού ολοκληρώσετε όλα τα σενάρια, το παιχνίδι τελειώνει με μια σειρά ερωτήσεων γύρω από τα ζητήματα της ταυτότητας σε όλη την Ευρώπη. Αφού απαντήσετε σε αυτά, ολοκληρώσατε το εκπαιδευτικό παιχνίδι της ΕΣ της ΕΕ. Σας ευχαριστούμε που επιλέξατε αυτό το παιχνίδι και καλή τύχη, ελπίζουμε να σας αρέσει να παίζετε!");
 
+        //Introductory Text for German being set in PlayerPrefs
+        PlayerPrefs.SetString("IntroductoryGerman", "[INSERT TEXT HERE]");
+
+        //Introductory Text for Dutch being set in PlayerPrefs
+        PlayerPrefs.SetString("IntroductoryDutch", "[INSERT TEXT HERE]");
+
+        //Introductory Text for Croatian being set in PlayerPrefs
+        PlayerPrefs.SetString("IntroductoryCroatian", "[INSERT TEXT HERE]");
+
+        //Reading all previous set PlayerPrefs so they can be set depending on the bool state of each language in the Update()
         introductoryEN = PlayerPrefs.GetString("IntroductoryEnglish");
         introductoryGR = PlayerPrefs.GetString("IntroductoryGreek");
     }
@@ -42,6 +68,39 @@ public class Introduction : MonoBehaviour
 
         if (greek) {
             introductoryText.text = introductoryGR;
+        }
+
+        if (german) {
+            introductoryText.text = introductoryDE;
+        }
+
+        if (dutch) {
+            introductoryText.text = introductoryNL;
+        }
+
+        if (croatian) {
+            introductoryText.text = introductoryHR;
+        }
+    }
+
+    public void Continue() {
+        //continue after pressing -> button
+        SceneManager.LoadScene("Start");
+    }
+
+    int boolToInt(bool val) {
+        if (val) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    bool intToBool(int val) {
+        if (val != 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- LogInRegister/Assets/Scripts/LanguageSelection.cs
+++ LogInRegister/Assets/Scripts/LanguageSelection.cs
@@ -2,11 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LanguageSelection : MonoBehaviour
 {
 
-    public Image SelectionLanguage;
+    private bool english = false;
+    private bool greek = false;
+    private bool german = false;
+    private bool dutch = false;
+    private bool croatian = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Setting PlayerPrefs back so it effectively works
+        english = false;
+        PlayerPrefs.SetInt("EnglishSelected", boolToInt(english));
+
+        greek = false;
+        PlayerPrefs.SetInt("GreekSelected", boolToInt(greek));
+
+        german = false;
+        PlayerPrefs.SetInt("GermanSelected", boolToInt(german));
+
+        dutch = false;
+        PlayerPrefs.SetInt("DutchSelected", boolToInt(dutch));
+
+        croatian = false;
+        PlayerPrefs.SetInt("CroatianSelected", boolToInt(croatian));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void English() {
+        english = true;
+        PlayerPrefs.SetInt("EnglishSelected", boolToInt(english));
+        Debug.Log(PlayerPrefs.GetInt("EnglishSelected"));
+        SceneManager.LoadScene("OpeningIntroduction");
+    }
+
+    public void Greek() {
+        greek = true;
+        PlayerPrefs.SetInt("GreekSelected", boolToInt(greek));
+        SceneManager.LoadScene("OpeningIntroduction");
+    }
+
+    public void German() {
+        german = true;
+        PlayerPrefs.SetInt("GermanSelected", boolToInt(german));
+        SceneManager.LoadScene("OpeningIntroduction");
+    }
+
+    public void Dutch() {
+        dutch = true;
+        PlayerPrefs.SetInt("DutchSelected", boolToInt(dutch));
+        SceneManager.LoadScene("OpeningIntroduction");
+    }
+
+    public void Croatian() {
+        croatian = true;
+        PlayerPrefs.SetInt("CroatianSelected", boolToInt(croatian));
+        SceneManager.LoadScene("OpeningIntroduction");
+    }
+
+    //These conversion functions allow me to tell the games language through playerprefs
+    int boolToInt(bool val) {
+        if (val) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    bool intToBool(int val) {
+        if (val != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*public Image SelectionLanguage;
     public List<Sprite> FlagList = new List<Sprite>();
     private int flagSpot = 0;
 
@@ -33,17 +114,5 @@ public class LanguageSelection : MonoBehaviour
                 SelectionLanguage.sprite = FlagList[flagSpot];
             }
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }*/
 }
